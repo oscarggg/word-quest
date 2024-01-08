b870573a8f3a90532d7c773917e384afcb309a14
oscarggg <oscarg2062009@gmail.com>
2024-01-08 02:16:17 -0800

    express js server setup

---
+++ server/src/app.js
@@ -0,0 +1,22 @@
+console.log('yoo')
+
+// goes into node_modules folder, searches for package 'express', searches for "main" file and retrieves it into variable
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+// builds application server
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+// status endpoint
+app.get('/status', (req, res) => {
+    res.send({
+        message: 'hello peasants'
+    })
+})
+
+app.listen(process.env.PORT || 5173)
